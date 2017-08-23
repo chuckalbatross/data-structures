b5206fac901831661a6e0f3b020e7bea2ec8d20d
Helen Yi <helenhyi@gmail.com>
2017-08-23 16:13:43 -0700

    Completed tests for functional-shared queue and stack.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  
+  var someInstance = {};
+  someInstance.storage = {};
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () {
+    return Object.keys(this.storage).length;
+  },
+  enqueue: function (value) {
+    this.storage[value] = value;
+  },
+  dequeue: function () {
+    var dequeuedValue = this.storage[Object.keys(this.storage)[0]];
+    delete this.storage[Object.keys(this.storage)[0]];
+    return dequeuedValue;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,7 +3,6 @@ var Stack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
   someInstance.storage = {};
-  // var testString = { key: 'hello'};
 
   _.extend(someInstance, stackMethods);
 
@@ -11,15 +10,15 @@ var Stack = function() {
 };
 
 var stackMethods = {
-	size : function() {
-	 return Object.keys(this.storage).length;
-	},
-	push : function(value) {
-      this.storage[Object.keys(this.storage).length] = value;
-	},
-	pop : function() {
-      var poppedValue = this.storage[Object.keys(this.storage).length - 1];
-      delete this.storage[Object.keys(this.storage).length - 1];
-      return poppedValue;
-	}
+  size : function() {
+    return Object.keys(this.storage).length;
+  },
+  push : function(value) {
+    this.storage[Object.keys(this.storage).length] = value;
+  },
+  pop : function() {
+    var poppedValue = this.storage[Object.keys(this.storage).length - 1];
+    delete this.storage[Object.keys(this.storage).length - 1];
+    return poppedValue;
+  }
 };
