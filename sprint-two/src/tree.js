e4e0123d963ab847b4bc972bc7b9fe9445e65aab
Helen Yi <helenhyi@gmail.com>
2017-08-24 11:38:09 -0700

    Completed tests for tree.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+
+  newTree.children = []; 
 
   return newTree;
 };
@@ -11,13 +12,28 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));	
 };
 
 treeMethods.contains = function(target) {
+  var containsRecurse = function (tree) {
+    if (tree.value === target) {
+      return true;
+    }
+    for (var i = 0; i < tree.children.length; i++) {
+      if (containsRecurse(tree.children[i])) {
+        return true;
+      }
+    }
+    return false;
+  };
+  return containsRecurse(this);
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild - O(1)
+ contains - O(n)
  */
