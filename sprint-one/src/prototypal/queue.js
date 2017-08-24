ab106b1f3ddeb051dbd1aea98589c37addbc2c2b
James Bonner <jbonner89@gmail.com>
2017-08-23 19:33:39 -0700

    complete stack and queue implementation using prototypal instantiation pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,23 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+    return Object.keys(this.storage).length;
+  },
+  enqueue: function(value) {
+    this.storage[value] = value;
+  },
+  dequeue: function() {
+    var dequeuedVal = this.storage[Object.keys(this.storage)[0]];
+    delete this.storage[Object.keys(this.storage)[0]];
+    return dequeuedVal;
+  }
+};
 
 
